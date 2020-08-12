5e673f77f14ddc83f1c69dac937916947cd8206f
hushin <nnnnot+github@gmail.com>
2020-08-12 22:22:38 +0900

    fix tmux theme

--- config/.config/tmux/themes/mytheme.sh
+++ config/.config/tmux/themes/mytheme.sh
@@ -27,9 +27,9 @@ if [ -z $TMUX_POWERLINE_LEFT_STATUS_SEGMENTS ]; then
 		"hostname 33 0" \
 		#"ifstat 30 255" \
 		#"ifstat_sys 30 255" \
-		"lan_ip 24 255 ${TMUX_POWERLINE_SEPARATOR_RIGHT_THIN}" \
-		"wan_ip 24 255" \
-		"vcs_branch 29 88" \
+		# "lan_ip 24 255 ${TMUX_POWERLINE_SEPARATOR_RIGHT_THIN}" \
+		# "wan_ip 24 255" \
+		"vcs_branch 54 139" \
 		"vcs_compare 60 255" \
 		"vcs_staged 64 255" \
 		"vcs_modified 9 255" \
@@ -40,18 +40,18 @@ fi
 if [ -z $TMUX_POWERLINE_RIGHT_STATUS_SEGMENTS ]; then
 	TMUX_POWERLINE_RIGHT_STATUS_SEGMENTS=(
 		#"earthquake 3 0" \
-		"pwd 89 211" \
+		# "pwd 89 211" \
 		#"macos_notification_count 29 255" \
-		"mailcount 9 255" \
-		"now_playing 234 37" \
-		#"cpu 240 136" \
-		"load 237 167" \
-		#"tmux_mem_cpu_load 234 136" \
-		"battery 137 127" \
-		"weather 37 255" \
+		# "mailcount 9 255" \
+		# "now_playing 234 37" \
+		# "cpu 240 136" \
+		# "load 237 167" \
+		"tmux_mem_cpu_load 234 136" \
+		# "battery 137 127" \
+		# "weather 37 255" \
 		#"rainbarf 0 ${TMUX_POWERLINE_DEFAULT_FOREGROUND_COLOR}" \
 		#"xkb_layout 125 117" \
-		"date_day 235 136" \
+		# "date_day 235 136" \
 		"date 235 136 ${TMUX_POWERLINE_SEPARATOR_LEFT_THIN}" \
 		"time 235 136 ${TMUX_POWERLINE_SEPARATOR_LEFT_THIN}" \
 		#"utc_time 235 136 ${TMUX_POWERLINE_SEPARATOR_LEFT_THIN}" \
