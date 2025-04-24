e3940ed71998fa7c30475daaa3c2bb2665c8a1f5
hushin <ngtv.hsn@gmail.com>
2025-04-24 23:39:20 +0900

    feat: config.fish設定追加

--- config/.config/fish/config.fish
+++ config/.config/fish/config.fish
@@ -1,3 +1,10 @@
+# VSCodeで直接fishを開いてpathを通すための設定
+bass source ~/.profile
+
 . ~/.config/fish/env.fish
 . ~/.config/fish/aliases.fish
 . ~/.config/fish/keybinds.fish
+
+# bun FIXME .profileに移す
+set --export BUN_INSTALL "$HOME/.bun"
+set --export PATH $BUN_INSTALL/bin $PATH
